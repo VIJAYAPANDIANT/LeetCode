5b8706e695f02e4a65a49452a3b9d6707669a50f
VIJAYAPANDIANT <vijayapandian112007@gmail.com>
2026-01-01 23:58:26 +0530

    vj

--- Medium/Day18.java
+++ Medium/Day18.java
@@ -11,7 +11,9 @@ Example 2:
 
 Input: n = 1
 Output: ["()"] */
-/*class Solution {
+/*import java.util.*;
+
+class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
         backtrack(result, "", 0, 0, n);
@@ -19,37 +21,21 @@ Output: ["()"] */
     }
 
     private void backtrack(List<String> result, String current, int open, int close, int max) {
+        // If the current string is complete
         if (current.length() == max * 2) {
             result.add(current);
             return;
         }
 
+        // Add '(' if we still can
         if (open < max) {
             backtrack(result, current + "(", open + 1, close, max);
         }
+
+        // Add ')' only if it keeps the string valid
         if (close < open) {
             backtrack(result, current + ")", open, close + 1, max);
         }
     }
 }
  */
-/*class Solution {
-    public List<String> generateParenthesis(int n) {
-        List<String> result = new ArrayList<>();
-        backtrack(result, "", 0, 0, n);
-        return result;
-    }
-    private void backtrack(List<String> result, String current, int open, int close, int max) {
-        if (current.length() == max * 2) {
-            result.add(current);
-            return;
-        }
-        if (open < max) {
-            backtrack(result, current + "(", open + 1, close, max);
-        }
-        if (close < open) {
-            backtrack(result, current + ")", open, close + 1, max);
-        }
-    }
-} */
-
