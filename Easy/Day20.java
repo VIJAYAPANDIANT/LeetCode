b70dd0f3cd44d96aa9539130fbba7d16cf029a78
VIJAYAPANDIANT <vijayapandian112007@gmail.com>
2026-01-03 19:34:48 +0530

    vj

---
+++ Easy/Day20.java
@@ -0,0 +1,38 @@
+/*119. Pascal's Triangle II
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+Example 1:
+
+Input: rowIndex = 3
+Output: [1,3,3,1]
+Example 2:
+
+Input: rowIndex = 0
+Output: [1]
+Example 3:
+
+Input: rowIndex = 1
+Output: [1,1] */
+/*import java.util.*;
+
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+
+        for (int i = 1; i <= rowIndex; i++) {
+            // update from right to left
+            for (int j = i - 1; j > 0; j--) {
+                row.set(j, row.get(j) + row.get(j - 1));
+            }
+            row.add(1);
+        }
+        return row;
+    }
+}
+ */
+/*🕒 Time & Space Complexity
+Metric	Complexity
+Time	O(rowIndex²)
+Space	O(rowIndex) */
