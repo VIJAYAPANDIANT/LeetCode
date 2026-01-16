40d260108d911ed1b8326b2d92b37570e8ffafde
VIJAYAPANDIANT <vijayapandian112007@gmail.com>
2026-01-16 19:01:34 +0530

    main

---
+++ Easy/Day33.java
@@ -0,0 +1,43 @@
+/*70. Climbing Stairs
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step */
+/*class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+
+        int prev2 = 1; // ways to reach step 1
+        int prev1 = 2; // ways to reach step 2
+
+        for (int i = 3; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
+ */
+/*⏱ Time & Space Complexity
+Complexity	Value
+Time	O(n)
+Space	O(1) */
